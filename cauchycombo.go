e9c9340f93772968db62d5a87f621b6d4c08214b
Bruce Ediger <bediger8@gmail.com>
2025-07-10 18:25:11 -0600

    command line flags for both distribution's params

--- cauchycombo.go
+++ cauchycombo.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"time"
@@ -9,16 +10,24 @@ import (
 )
 
 func main() {
+	g1 := flag.Float64("g1", 1.0, "first distribution's gamma value")
+	g2 := flag.Float64("g2", 1.0, "second distribution's gamma value")
+	l1 := flag.Float64("l1", 0.0, "first distribution's location")
+	l2 := flag.Float64("l2", 0.0, "second distribution's location")
+	n := flag.Int("n", 100000, "number of samples of cauchy distributions")
 
-	gamma1 := 1.0
-	gamma2 := 1.0
-	location1 := 0.0
-	location2 := 2.0
+	flag.Parse()
+
+	gamma1 := *g1
+	gamma2 := *g2
+	location1 := *l1
+	location2 := *l2
+	iterations := *n
 
 	crng1 := rng.NewCauchyGenerator(time.Now().UnixNano() + int64(os.Getpid()))
-	crng2 := rng.NewCauchyGenerator(time.Now().UnixNano() + int64(os.Getpid()))
+	crng2 := rng.NewCauchyGenerator(time.Now().UnixNano())
 
-	for i := 0; i < 100000; i++ {
+	for i := 0; i < iterations; i++ {
 		a := crng1.Cauchy(location1, gamma1)
 		b := crng2.Cauchy(location2, gamma2)
 
