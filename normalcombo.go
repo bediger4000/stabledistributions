e58e0219fc3cbd093f066c55825ca646e1a96e32
Bruce Ediger <bediger8@gmail.com>
2025-07-10 18:52:57 -0600

    both distributions params as command line args

--- normalcombo.go
+++ normalcombo.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 )
 
 func main() {
-	mean1 := 0.0
-	stdDev1 := 1.0
-	mean2 := 0.0
-	stdDev2 := 3.0
+	m1 := flag.Float64("m1", 0.0, "first distribution's mean")
+	m2 := flag.Float64("m2", 1.0, "second distribution's mean")
+	s1 := flag.Float64("s1", 1.0, "first distribution's standard deviation")
+	s2 := flag.Float64("s2", 3.0, "second distribution's standard deviation")
+	itr := flag.Int("i", 100000, "numer of normally distributed samples")
 
-	for i := 0; i <= 100000; i++ {
+	flag.Parse()
+
+	mean1 := *m1
+	mean2 := *m2
+	iterations := *itr
+	stdDev1 := *s1
+	stdDev2 := *s2
+
+	for i := 0; i <= iterations; i++ {
 		f := (rand.NormFloat64()*stdDev1 + mean1)
 		g := (rand.NormFloat64()*stdDev2 + mean2)
-		fmt.Printf("%.06f\n", f+g)
+		fmt.Printf("%.06f\t%.05f\t%.05f\n", f, g, f+g)
 	}
 }
