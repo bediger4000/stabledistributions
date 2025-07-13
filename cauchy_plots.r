30924126681ca929fcb7bbd0afeb2f1272cfe680
Bruce Ediger <bediger8@gmail.com>
2025-07-12 21:57:27 -0600

    100 breaks in combined histogram

--- cauchy_plots.r
+++ cauchy_plots.r
@@ -14,11 +14,11 @@ dev.off()
 png("cauchy_combo.png", width=750, height=600)
 hist(a, breaks=75, freq=F, main="Combined Cauchy Distributions", xlab="bin center value", angle=45, col="green", density=20)
 hist(b, breaks=75, freq=F, angle=-45, col="orange", density=20, add=T)
-hist(ab, breaks=75, freq=F, angle=30, col="blue", density=20, add=T)
+hist(ab, breaks=100, freq=F, angle=30, col="blue", density=20, add=T)
 dev.off()
 
 png("cauchy_combo_pdf.png", width=750, height=600)
-hist(ab, breaks=75, freq=F, main="Combined Cauchy Distribution and Probability Density curve fit", xlab="bin center value", angle=30, col="blue", density=20)
+hist(ab, breaks=100, freq=F, main="Combined Cauchy Distribution and Probability Density curve fit", xlab="bin center value", angle=30, col="blue", density=20)
 curve(dcauchy(x, location=4.8, scale= 2.9), add=T, col="red", lwd=1)
 dev.off()
 
