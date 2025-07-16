3338af2b30449102d5193fd9b1b024124f4718f4
Bruce Ediger <bediger8@gmail.com>
2025-07-16 05:56:18 -0600

    normal distribution for sum of 5 uniform distributions

--- multi_plots.r
+++ multi_plots.r
@@ -4,5 +4,5 @@ s <- df$sum
 
 png("multi_uniform.png", width=750, height=600)
 hist(s, breaks=100, freq=F, main="Density of uniform distribution sum", xlab="bin center value")
-curve(dnorm(x, 7.038, 1.980), add=T, col="red", lwd=2)
+curve(dnorm(x, 2.5, .644), add=T, col="red", lwd=2)
 dev.off()
